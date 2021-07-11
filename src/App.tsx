177df25b1cf3f88c94c7e196249aaaf7d1fe7897
Ahmed Nouira <ahmnouira@gmail.com>
2021-07-11 19:22:50 +0100

    feat: start project

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import {ProfileScreen} from './pages/ProfileScreen';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -45,7 +45,7 @@ const App: React.FC = () => (
             <Tab2 />
           </Route>
           <Route path="/tab3">
-            <Tab3 />
+            <ProfileScreen />
           </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
@@ -54,7 +54,7 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon icon={ellipse} />
@@ -62,7 +62,7 @@ const App: React.FC = () => (
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonLabel>Profile</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
